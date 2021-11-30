4c05e7eb0ca33bfc21413b518fe2b26d876fa2cb
Atticus Yong <atticusy28965@gmail.com>
2021-11-29 16:41:03 -0800

    lesson-1.2

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('hello, Vue');
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 45
+        }
+    },
+    methods: {
+        changeTitle(title) {
+            // this.title = 'Words of Radiance'
+            this.title = title
+        }
+    },
+})
+
+app.mount('#app')
