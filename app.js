33588cff34fa40c460447d7b72340e27f61c1a88
Atticus Yong <atticusy28965@gmail.com>
2021-12-01 15:43:58 -0800

    lesson-3

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45
+            showBooks: true,
+            books: [
+                {title: 'Name of the Wind', author: 'Patrick Rothfuss'},
+                {title: 'The Way of Kings', author: 'Brandon Sanderson'},
+                {title: 'The Final Empire', author: 'Brandon Sanderson'},
+            ]
         }
     },
+    methods: {
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        },
+    }
 })
 
 app.mount('#app')
